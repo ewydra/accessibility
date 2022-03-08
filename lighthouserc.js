1b562f6723ee5415227480dd071a6cbc195a32a8
Ewa Wydra <e.wydra@selleo.com>
2022-03-08 22:16:51 +0100

    Add lighthouse run on CI

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,14 @@
 module.exports = {
   ci: {
+    collect: {
+      url: ["http://localhost:3000/"],
+      startServerCommand: "npm start",
+    },
     upload: {
       target: "temporary-public-storage",
+    },
+    assert: {
+      preset: "lighthouse:no-pwa",
     },
   },
 };
