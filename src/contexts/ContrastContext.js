2c35a79da7970503ba5d911bc6a9f8e4ef247533
Ewa Wydra <e.wydra@selleo.com>
2022-03-19 11:55:48 +0100

    Add accessibility adjustments

--- src/contexts/ContrastContext.js
+++ src/contexts/ContrastContext.js
@@ -5,10 +5,15 @@ const ContrastContext = React.createContext();
 export const useContrastMode = () => useContext(ContrastContext);
 
 export const ContrastProvider = ({ children }) => {
-  const [isHighContrastMode, setHighContrastMode] = useState(false);
+  const [isHighContrastMode, setHighContrastMode] = useState(
+    () => localStorage.getItem("contrastMode") === "true"
+  );
 
   const changeContrastMode = useCallback(() => {
-    setHighContrastMode((prev) => !prev);
+    setHighContrastMode((prev) => {
+      localStorage.setItem("contrastMode", !prev);
+      return !prev;
+    });
   }, []);
 
   return (
